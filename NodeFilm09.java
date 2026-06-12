f6e342f7b81b2ede0c4b94d0c54bf27b30182874
evan1234-map <evanfernanda1708@gmail.com>
2026-06-12 10:08:56 +0700

    Class NodeFilm

---
+++ NodeFilm09.java
@@ -0,0 +1,11 @@
+public class NodeFilm09 {
+    Film09 data;
+    NodeFilm09 left;
+    NodeFilm09 right;
+
+    public NodeFilm09(Film09 data)  {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
